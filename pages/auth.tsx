0dc2eff96f4b22282bcf7631f1a5a5dfd04c0cdd
Sody15 <pdsoderb@gmail.com>
2023-04-21 15:35:37 -0700

    Added head tag to auth page

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,10 +1,22 @@
 import { AuthForm } from '@/components';
+import Head from 'next/head';
 
 const AuthPage = () => {
   return (
-    <div className='flex justify-center mt-10 -mx-6'>
-      <AuthForm />
-    </div>
+    <>
+      <Head>
+        <title>Todo- Auth</title>
+        <meta charSet='utf-8' />
+        <meta name='viewport' content='initial-scale=1.0, width=device-width, maximum-scale=1.0' />
+        <meta
+          name='description'
+          content='Auth page for simple todo app that helps you keep track of all those little important things in life.'
+        />
+      </Head>
+      <div className='flex justify-center mt-10 -mx-6'>
+        <AuthForm />
+      </div>
+    </>
   );
 };
 
