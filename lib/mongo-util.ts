22d0810d92096ef73a0fbb54fe6f1d404d4248ee
Sody15 <pdsoderb@gmail.com>
2023-04-21 18:09:34 -0700

    testing

--- lib/mongo-util.ts
+++ lib/mongo-util.ts
@@ -2,11 +2,12 @@ import { Db, MongoClient } from 'mongodb';
 
 class MongoUtil {
   // Define static properties for the client and database instances.
+  private static db: Db | null = null;
   private static client: MongoClient;
-  private static db: Db;
 
   // Define a static method for getting a reference to the database instance.
   static async getDb(): Promise<Db> {
+    console.log('db is - ' + MongoUtil.db);
     if (!MongoUtil.db) {
       console.log('Create connection');
       MongoUtil.db = await MongoClient.connect(<string>process.env.MONGODB_URI)
@@ -15,6 +16,7 @@ class MongoUtil {
           throw new Error();
         });
     }
+
     return new Promise((resolve, reject) => {
       if (MongoUtil.db) {
         resolve(MongoUtil.db);
